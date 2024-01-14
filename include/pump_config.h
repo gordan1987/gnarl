84950113132b381760fc15f87e4798632a7f327e
gordan1987 <gor1987dan@gmail.com>
2024-01-14 23:21:16 +0200

    Update pump_config.h

--- include/pump_config.h
+++ include/pump_config.h
@@ -1,3 +1,3 @@
 #define PUMP_ID         "819741"        // pump serial number (note that this is a string constant)
-#define PUMP_FREQUENCY  868000000       // pump frequency
-#define MMTUNE_START    868000000       // starting frequency for mmtune scans
+#define PUMP_FREQUENCY  868350000       // pump frequency
+#define MMTUNE_START    868350000       // starting frequency for mmtune scans
