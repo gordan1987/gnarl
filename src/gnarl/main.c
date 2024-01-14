fcddc7099a33415c4ff7d6074b160f5d53da187d
gordan1987 <gor1987dan@gmail.com>
2024-01-14 23:32:07 +0200

    Update main.c

--- src/gnarl/main.c
+++ src/gnarl/main.c
@@ -7,6 +7,13 @@
 #include "rfm95.h"
 
 void app_main(void) {
+	   esp_task_wdt_config_t wdtc;
+   wdtc.idle_core_mask = 0;
+   wdtc.idle_core_mask |= (1 << 0);
+   wdtc.idle_core_mask |= (1 << 1);
+   wdtc.timeout_ms = WDT_TIMEOUT_SECONDS;
+   wdtc.trigger_panic = true;
+   esp_task_wdt_init(&wdtc);
 	ESP_LOGI(TAG, "%s", SUBG_RFSPY_VERSION);
 	rfm95_init();
 	uint8_t v = read_version();
